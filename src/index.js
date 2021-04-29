55d062d4193958a304567a06102e6ff8e9a24801
erelle <reina.boubli1998@gmail.com>
2021-04-29 13:52:06 +0300

    ftgyhu

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 const express=require('express')
 const port  =process.env.PORT || 4000
-const connectDB=require('./DB/connection.js');
+const connectDB=require('../DB/connection.js');
 
 const app  =express()
 
 connectDB();
 
-app.use('/api/userModel', require('./Api/User'));
+app.use('/api/userModel', require('../Api/User'));
 
 app.use(express.static(__dirname));
 app.use(express.static("public"));
 
-var path=require('path');
+const path = require('path');
 
 app.get('/',(req ,res)=>{
     res.sendFile(path.join(__dirname)+"/src/HomePage.html")
@@ -23,7 +23,6 @@ app.get('/LoginPage',(req ,res)=>{
 
 app.get('/RegisterPage',(req ,res)=>{
     res.sendFile(path.join(__dirname)+"/src/RegisterPage.html")
-    var name= req.body.firstName;
 })
 
 app.listen(port,()=>{
